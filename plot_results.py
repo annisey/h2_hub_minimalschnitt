83ab0e691fbd0926fd346a294a86d7505c1087db
ann82611 <ann.kathrin.seyfried@ict.fraunhofer.de>
2024-09-09 16:03:21 +0200

    added a main function so that the files is also executable itself and not just by being called by hydrogen_hub.py

--- plot_results.py
+++ plot_results.py
@@ -1,11 +1,13 @@
-from oemof.solph import views
+from oemof.solph import views, processing
 # import pprint as pp
 import matplotlib.pyplot as plt
+import oemof
 
 
-def plot_figures_for(element):
+def plot_figures_for(element, title):
     figure, axes = plt.subplots(figsize=(10, 5))
     element["sequences"].plot(ax=axes, kind="line", drawstyle="steps-post")
+    plt.title(title)
     plt.legend(
         loc="upper center",
         prop={"size": 8},
@@ -23,6 +25,17 @@ def plot_result(energy_system):
     h2_storage = views.node(main_results, 'h2_storage')
     electricity_bus = views.node(main_results, 'electricity')
 
-    plot_figures_for(electricity_bus)
+    plot_figures_for(electricity_bus, "Electricity Bus")
+    plot_figures_for(h2_storage, "H2 Bus")
+    # print(energy_system)
+    #print(main_results)
 
+def main():
+    energy_system = oemof.solph.EnergySystem()
+    energy_system.restore('C:\\Users\\ann82611\\ownCloud\\U-Platte\\04_Code\\hydrogen_hub\\h2_hub_minimalschnitt\\h2_hub_dumps', 'h2_hub_dump.oemof')
     
+    plot_result(energy_system)
+
+
+if __name__ == "__main__":
+    main() 
